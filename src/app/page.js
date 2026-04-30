1b87a544c348a4d5f7e05e2cd25f84032cc76d40
salauddin <salauddin@hasan.com>
2026-04-30 15:25:43 +0600

    feat: add Hero Banner with stats section

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import Banner from "@/components/Banner";
+
 export default function Home() {
   return (
     <div>
-      <h2 className="text-5xl text-center">Home Page</h2>
+      <Banner />
     </div>
   );
 }
